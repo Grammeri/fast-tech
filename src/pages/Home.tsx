8a549571048921fc91504f1d49a4eb9c7ae3c609
Dmitry Nikolayev <reacttsxgrammer@gmail.com>
2025-08-28 00:26:16 +0300

    feat: create home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Home: React.FC = () => {
+    return (
+        <div style={{ padding: 16 }}>
+            <h1>Home</h1>
+            <p>Тут позже сделаем 5 картинок с параллельной загрузкой и автообновлением каждые 10 секунд.</p>
+        </div>
+    );
+};
