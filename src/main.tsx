660ce46384445bf445a851e15b9e2f4e1e374779
Dmitry Nikolayev <reacttsxgrammer@gmail.com>
2025-08-28 10:43:00 +0300

    feat: wrap app with AuthProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router';
-
+import { AuthProvider } from '@/shared/providers/AuthProvider';
 import './styles/global.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <AuthProvider>
+            <RouterProvider router={router} />
+        </AuthProvider>
     </React.StrictMode>,
 );
-
