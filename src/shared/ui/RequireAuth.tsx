eaca587b4add99cf9c51aa6917a96fb954bc6bff
Dmitry Nikolayev <reacttsxgrammer@gmail.com>
2025-08-28 10:39:41 +0300

    feat: refactor: fix AuthProvider

--- src/shared/ui/RequireAuth.tsx
+++ src/shared/ui/RequireAuth.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
-import { AuthContext } from '@/shared/providers/AuthProvider';
 import { authStorage } from '@/shared/lib/auth';
+import {AuthContext} from "@/shared/contexts/AuthContext.ts";
 
 export const RequireAuth: React.FC = () => {
     const { isAuth } = useContext(AuthContext);
